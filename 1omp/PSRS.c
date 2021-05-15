3f447b46ea4a610319226c1ad1e45a7a55a286c4
totoro <yyw953834564@gmail.com>
2021-05-15 10:48:37 +0800

    lab1 ok

--- 1omp/PSRS.c
+++ 1omp/PSRS.c
@@ -2,142 +2,140 @@
 #include <string.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <math.h>
 
 #define num_threads 3
+#define DBL_MAX 65535
 
-int *L, *R;
-
-//Merge函数合并两个子数组形成单一的已排好序的字数组
-//并代替当前的子数组A[p..r]
-void Merge(int *a, int p, int q, int r)
+void swap(int *a, int *b)
 {
-    int i, j, k;
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    L = (int *)malloc((n1 + 1) * sizeof(int));
-    R = (int *)malloc((n2 + 1) * sizeof(int));
-    for (i = 0; i < n1; i++)
-        L[i] = a[p + i];
-    L[i] = 65536;
-    for (j = 0; j < n2; j++)
-        R[j] = a[q + j + 1];
-    R[j] = 65536;
-    i = 0, j = 0;
-    for (k = p; k <= r; k++)
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+int partition(int array[], int low, int high)
+{
+    int pivot = array[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++)
     {
-        if (L[i] <= R[j])
+        if (array[j] <= pivot)
         {
-            a[k] = L[i];
             i++;
-        }
-        else
-        {
-            a[k] = R[j];
-            j++;
+            swap(&array[i], &array[j]);
         }
     }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
 }
-//归并排序
-void MergeSort(int *a, int p, int r)
+
+void quickSort(int array[], int low, int high)
 {
-    if (p < r)
+    if (low < high)
     {
-        int q = (p + r) / 2;
-        MergeSort(a, p, q);
-        MergeSort(a, q + 1, r);
-        Merge(a, p, q, r);
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
     }
 }
 
-void PSRS(int *array, int n)
+void copy_seq(int *from, int *to, int len)
 {
-    int id;
-    int i = 0;
-    int count[num_threads][num_threads] = {0};           //每个处理器每段的个数
-    int base = n / num_threads;                          //划分的每段段数
-    int p[num_threads * num_threads];                    //正则采样数为p
-    int pivot[num_threads - 1];                          //主元
-    int pivot_array[num_threads][num_threads][50] = {0}; //处理器数组空间
+    for (int i = 0; i < len; i++)
+        to[i] = from[i];
+}
 
-    omp_set_num_threads(num_threads);
-#pragma omp parallel shared(base, array, n, i, p, pivot, count) private(id)
+void PSRS(int *seq, int n)
+{
+    int k, m, step, i, p, j = 0, position[num_threads][num_threads + 1], len[num_threads];
+    int sample[num_threads * num_threads], sample_main[num_threads], copy[n + num_threads - 1];
+    if (n <= 1)
+        return;
+    p = num_threads;
+    while (p * p > n)
     {
+        p--;
+    }
+    k = int(ceil((double)n / p));
+    m = k * p - n;
+    copy_seq(seq, copy, n);
+    for (i = 0; i < m; i++)
+        copy[n + i] = DBL_MAX;
+    step = int(floor((double)k / p));
+    omp_set_num_threads(p);
+#pragma omp parallel private(i)
+    {
+        int id;
         id = omp_get_thread_num();
-        MergeSort(array, id * base, (id + 1) * base - 1); //每个处理器对所在的段进行局部串行归并排序
-
-        // for (int i = 0; i < n; i++)
-        //     printf("%d ", array[i]);
-
+        for (i = 0; i < p; i++)
+        {
+            quickSort(copy, id * k, id * k + k - 1);
+            sample[id * p + i] = copy[id * k + step * i];
+        }
 #pragma omp critical
-        //每个处理器选出P个样本，进行正则采样
-        for (int k = 0; k < num_threads; k++)
-            p[i++] = array[(id - 1) * base + (k + 1) * base / (num_threads + 1)];
-//设置路障，同步队列中的所有线程
-#pragma omp barrier
-//主线程对采样的p个样本进行排序
-#pragma omp master
         {
-            MergeSort(p, 0, i - 1);
-            //选出num_threads-1个主元
-            for (int m = 0; m < num_threads - 1; m++)
-                pivot[m] = p[(m + 1) * num_threads];
-        }
-#pragma omp barrier
-        //根据主元对每一个cpu数据段进行划分
-        for (int k = 0; k < base; k++)
-        {
-            for (int m = 0; m < num_threads; m++)
-            {
-                if (array[id * base + k] < pivot[m])
-                {
-                    pivot_array[id][m][count[id][m]++] = array[id * base + k];
-                    break;
-                }
-                else if (m == num_threads - 1) //最后一段
-                {
-                    pivot_array[id][m][count[id][m]++] = array[id * base + k];
-                }
-            }
+            printf("\n%d:", id);
+            for (i = 0; i < k; i++)
+                printf("%d  ", (int)copy[id * k + i]);
         }
     }
-//向各个线程发送数据，各个线程自己排序
-#pragma omp parallel shared(pivot_array, count)
+    quickSort(sample, 0, p * p - 1);
+    printf("\n");
+    for (i = 0; i < p * p; i++)
+        printf("%d  ", (int)sample[i]);
+    for (i = 1; i < p; i++)
+        sample_main[i - 1] = sample[i * p];
+    sample_main[p - 1] = DBL_MAX - 1;
+    printf("\n");
+    for (i = 0; i < p - 1; i++)
+        printf("%d  ", (int)sample_main[i]);
+    omp_set_num_threads(p);
+#pragma omp parallel private(i, j) shared(position)
     {
-        int id = omp_get_thread_num();
-        for (int k = 0; k < num_threads; k++)
+        int id, start = 0, t;
+        id = omp_get_thread_num();
+        for (i = 0, j = 0, position[id][0] = 0; i < k; i++)
         {
-            if (k != id)
+            if (copy[id * k + i] > sample_main[j])
             {
-                memcpy(pivot_array[id][id] + count[id][id], pivot_array[k][id], sizeof(int) * count[k][id]);
-                count[id][id] += count[k][id];
+                j++;
+                position[id][j] = i;
             }
         }
-        MergeSort(pivot_array[id][id], 0, count[id][id] - 1);
-    }
-
-    // for (int i = 0; i < n; i++)
-    //     printf("%d ", array[i]);
-    i = 0;
-    printf("result:\n");
-    for (int k = 0; k < num_threads; k++)
-    {
-        for (int m = 0; m < count[k][k]; m++)
+        position[id][p] = k;
+//position[p-1][p]=k-m;
+#pragma omp barrier
+        for (i = 0; i < p; i++)
+            len[id] += position[i][id + 1] - position[i][id];
+#pragma omp barrier
+        for (i = 0; i < id; i++)
+            start += len[i];
+        for (i = 0, j = 0; i < p; i++)
         {
-            printf("%d ", pivot_array[k][k][m]);
+            printf("\nthread:%d,j=%d,start=%d,n=%d", omp_get_thread_num(), j, start, position[i][id + 1] - position[i][id]);
+            copy_seq(copy + i * k + position[i][id], seq + start + j, position[i][id + 1] - position[i][id]);
+            for (t = 0; t < position[i][id + 1] - position[i][id]; t++)
+                printf("  %d  ", (int)copy[i * k + position[i][id] + t]);
+            j = j + position[i][id + 1] - position[i][id];
         }
-        printf("\n");
+        //#pragma omp barrier
+        //position[p-1][p]=k-m;
+        quickSort(seq + start, 0, len[id] - 1);
     }
 }
+
 int main()
 {
     int array[27] = {15, 46, 48, 93, 39, 6, 72, 91, 14, 36, 69, 40, 89, 61, 97, 12, 21, 54, 53, 97, 84, 58, 32, 27, 33, 72, 20};
     double begin, end, time;
+
     begin = omp_get_wtime();
     PSRS(array, 27);
-    // MergeSort(array, 0, 26);
+    // quickSort(array, 0, 26);
     end = omp_get_wtime();
+
     time = end - begin;
-    printf("Final: ");
+    printf("\nFinal: ");
     for (int i = 0; i < 27; i++)
         printf("%d ", array[i]);
     printf("\nRunning time: %lfs\n", time);
